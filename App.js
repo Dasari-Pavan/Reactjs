ef34bf4dde4d995a1435af2e904b921798474c44
Dasari-Pavan <pvnkmrdsr@gmail.com>
2024-02-05 21:48:12 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,32 @@
+
+/** 
+ *     <div id="parent">
+ *             <div id="child1">
+ *              <h1>Im h1 tag</h1>
+ *              <h2></h2>im h2 tag</h2>
+ *             </div>
+ *         <div id="child2">
+ *              <h1>Im h1 tag</h1>
+ *              <h2></h2>im h2 tag</h2>
+ *             </div>
+ *      </div>
+ * 
+ * 
+ * 
+*/
+
+const parent=React.createElement("div",{id:"parent"},
+       [ React.createElement("div",{id:"child1"},
+        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+        ),
+        React.createElement("div",{id:"child2"},
+        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+        )]
+);
+     
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent);
+
+
+
