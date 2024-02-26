c4423c13cbcb000bce7de171fa94bd7786ce382b
Dasari-Pavan <pvnkmrdsr@gmail.com>
2024-02-26 11:24:31 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,18 +1,76 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div",{id:"parent"},
-       [ React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
-        ),
-        React.createElement("div",{id:"child2"},
-        [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
-        )]
+
+const Header=()=>{
+    return(
+    <div className="header">
+        <div className="Logo-container">
+            <img
+                className="logo"
+                src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg"/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
 );
-     
-const root=ReactDOM.createRoot(document.getElementById("root"))
+};
 
-root.render(parent);
+const Restaurantcard=()=>{
+   return(
+    <div className="res-card">
+        <img 
+        className="res-logo"
+        alt="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/0/19279320/60e414a5bac9e2c0cce19321dd838b71.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"/>
+     <h3>Meghana Chicken Biryani</h3>
+     <h4 id="bir">Biryani Andhra</h4>
+     <h4 id="rupees">₹300 for one</h4>
+    <div className="starrate">
+    <h4 id="rating">4.5 ⭐</h4>
+    <h4>30 minutes</h4>
+    </div>
+    </div>
+   );
+};
+
+
+const Body=()=>{
+    return(
+        <div className="body">
+            <div className="search">Search
+                <div className="Restaurant-Container">
+                   <Restaurantcard/>   
+                   <Restaurantcard/> 
+                   <Restaurantcard/> 
+                   <Restaurantcard/>
+                   <Restaurantcard/>
+                   <Restaurantcard/>
+                   <Restaurantcard/>
+                   <Restaurantcard/>
+                   
+                </div>
+            </div>
+        </div>
+    );
+};
 
 
 
+const AppLayout=()=>{
+    return(
+    <div className="app">
+        <Header/>
+        <Body></Body>
+    </div>
+);
+};
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 Reactjs 
+
+
+#Parcel
+-Dev Build
+-Local Server
+-HMR = Hot Module Replacement
+-File Watching Algorithm-written in c++
+-caching-faster Builds
+-Images optimization
+-Bundling
+-compressing
+-consistent Handling
+-code splitting
+-Differential Bundling -support older browsers
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking-remove unused code
+-Different dev and prod bundles
