8ec56e14aec8a00d3b8a802bbbe3883a91ac794b
Pavankumar <pvnkmrdsr@gmail.com>
2024-07-25 14:23:13 +0530

    Add files via upload
    Epsd 3.1

--- App.js
+++ App.js
@@ -1,76 +1,42 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDom from "react-dom/client"
+
+// React.createElement => object =>HTMLElement(render)
+
+// const heading=React.createElement("h1",{id:"heading"},"Parent element");
+
+// we need to jsx not  jsx =>react.createlement =>react.object => render htmml
+
+const jsxheading= <h1 className="ead" tabIndex="5">React using jsx syntax</h1>;
+
+// console.log(heading);
+// const root=ReactDom.createRoot(document.getElementById("root"));
+// root.render(jsxheading);
 
 
-const Header=()=>{
-    return(
-    <div className="header">
-        <div className="Logo-container">
-            <img
-                className="logo"
-                src="https://img.freepik.com/free-vector/detailed-chef-logo-template_23-2148986823.jpg"/>
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Contact us</li>
-                <li>Cart</li>
-            </ul>
-        </div>
-    </div>
-);
-};
+// React Element
+const Head=(
+  <h1 className="head" tabIndex="5">
+    Namaste React using react element
+  </h1>
+)
 
-const Restaurantcard=()=>{
-   return(
-    <div className="res-card">
-        <img 
-        className="res-logo"
-        alt="res-logo" src="https://b.zmtcdn.com/data/pictures/chains/0/19279320/60e414a5bac9e2c0cce19321dd838b71.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"/>
-     <h3>Meghana Chicken Biryani</h3>
-     <h4 id="bir">Biryani Andhra</h4>
-     <h4 id="rupees">₹300 for one</h4>
-    <div className="starrate">
-    <h4 id="rating">4.5 ⭐</h4>
-    <h4>30 minutes</h4>
-    </div>
-    </div>
-   );
-};
+// functional element
 
+const Heading=()=>(
+  <h1 className="head" tabIndex="5">
+    Namaste React using functional
+  </h1>
+)
+// functional component
+const HeadingComponent=()=>(
+  <div className="container">
+    {Head}
+    <Heading/>
+  <h1>Reactjs using Jsx via functional component</h1>
+  </div>
+)
 
-const Body=()=>{
-    return(
-        <div className="body">
-            <div className="search">Search
-                <div className="Restaurant-Container">
-                   <Restaurantcard/>   
-                   <Restaurantcard/> 
-                   <Restaurantcard/> 
-                   <Restaurantcard/>
-                   <Restaurantcard/>
-                   <Restaurantcard/>
-                   <Restaurantcard/>
-                   <Restaurantcard/>
-                   
-                </div>
-            </div>
-        </div>
-    );
-};
-
-
-
-const AppLayout=()=>{
-    return(
-    <div className="app">
-        <Header/>
-        <Body></Body>
-    </div>
-);
-};
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<AppLayout/>);
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
+// root.render(heading);
